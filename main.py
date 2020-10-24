52f5e5ec66660ae114f3032a21c16d214ca1b92b
Greg Robson <37176149+GregoryRobson@users.noreply.github.com>
2020-10-23 19:26:51 -0500

    Player Class Refactor
    Added in the player class and refactored the code to work with that. This results in a lot less global variables and a more streamlined system. Also changed the class from game_functons to game_functions lol, idk how we didn't notice that. Made some minor changes to test_functions as well as main to reflect the changes made elsewhere, and I'll note that I'm not 100% on the test_functions class, but it should be all okay. Besides the test_functions class though, everything has been tested and should work all the same from a user perspective as it did before.

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 """
 Upsetti Spaghetti Coders
 Kole Keeney, Chris Munns, Daylan Quinn and Greg Robson
-10/5/20
+10/23/20
 CS 449
 Nine Men's Morris main file
 """
 
 
 import pygame
-import game_functons
-
+import game_functions
 
 pygame.init()
 
@@ -17,7 +16,7 @@ pygame.init()
 pygame.display.set_caption("Nine Men's Morris")
 
 # Menu function allows for everything to be called as needed. This is where all the magic happens.
-game_functons.menu()
+game_functions.menu()
 
 # Done! Time to quit.
 pygame.quit()
