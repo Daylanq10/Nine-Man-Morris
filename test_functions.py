0d93de5672b4a700e2a32ec3f97d05e69420479e
Kole Keeney <33508520+kolek9@users.noreply.github.com>
2020-11-19 18:16:46 -0600

    Update test_functions.py
    Fixed a couple of the tests, the adjacency ones seem to be treating the board object like a list in the test for some reason. Fixed the create board call for those, though

--- test_functions.py
+++ test_functions.py
@@ -12,7 +12,7 @@ def test_user_swap_1():
     player_1 = player.Player(True, True)
     player_2 = player.Player(False, True)
     current = player_1.turn
-    game_functions.swap_player()
+    game_functions.swap_player(player_1, player_2)
     new = player_2.turn
     assert current == new == True
 
@@ -22,7 +22,7 @@ def test_user_swap_2():
     player_1 = player.Player(False, True)
     player_2 = player.Player(True, True)
     current = player_2.turn
-    game_functions.swap_player()
+    game_functions.swap_player(player_1, player_2)
     new = player_1.turn
     assert current == new == True
 
@@ -47,22 +47,22 @@ def test_adjacent_1():
 
 def test_adjacent_2():
     """Checks horizontal mill"""
-    test_grid = game_functions.create_board()
-
+    test_grid = game_functions.board.Board()
+    test_grid = test_grid.create_board()
     test_grid[0][0] = 1
     test_grid[0][3] = 1
     test_grid[0][6] = 1
 
-    assert game_functions.check_adjacent(x=0, y=6, board=test_grid, p=1) == 1
+    assert game_functions.check_adjacent(x=0, y=6, board=test_grid, player_piece=1) == 1
 
 
 def test_adjacent_3():
     """Checks vertical mill"""
-    test_grid = game_functions.create_board()
-
+    test_grid = board.Board()
+    test_grid = test_grid.create_board()
     test_grid[2][2] = 2
     test_grid[3][2] = 2
     test_grid[4][2] = 2
 
-    assert game_functions.check_adjacent(x=4, y=2, board=test_grid, p=2) == 1
+    assert game_functions.check_adjacent(x=4, y=2, board=test_grid, player_piece=2) == 1
 
